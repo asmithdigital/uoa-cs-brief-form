f75c23f472ad415cab562f35cec3fb0ca90af5f7
Andrew Smith <andrew.smith03@adelaide.edu.au>
2019-08-01 10:39:21 +0930

    add demo submit file from theme

--- js/steps-form.js
+++ js/steps-form.js
@@ -38,10 +38,35 @@ document.addEventListener("touchstart", function() {}, false);
         }
     });
 
+    // function submitForm() {
+    //     $("#mgsContactSubmit").html('');
+    //     $("#final-step-buttons").html('<div class="h3 text-center text-success"> You have finished all steps of this html form successfully!!! </div>');
+    //     swal("Good job!", "You have finished all steps of this html form successfully!!!", "success");
+    // }
     function submitForm() {
-        $("#mgsContactSubmit").html('');
-        $("#final-step-buttons").html('<div class="h3 text-center text-success"> You have finished all steps of this html form successfully!!! </div>');
-        swal("Good job!", "You have finished all steps of this html form successfully!!!", "success");
+        var form_data = new FormData($("#QuoteForm")[0]);
+        form_data.append('file', form_data);
+        $('#loading-image').show();
+        $('#final-step-buttons').hide();
+        $.ajax({
+            type: "POST",
+            url: "https://mgsdemo.mgscoder.com/mgscode/multi-step-form/quote-cond-multifile-attached.php",
+            data: form_data,
+            processData: false,
+            contentType: false,
+            success: function(text) {
+                if (text === "success") {
+                    formSuccess();
+                } else {
+                    formError();
+                    submitMSG(false, text);
+                }
+            },
+            complete: function() {
+                $('#loading-image').hide();
+                $('#final-step-buttons').show();
+            }
+        });
     }
     $(document).on('change', ':file', function() {
         var input = $(this),
